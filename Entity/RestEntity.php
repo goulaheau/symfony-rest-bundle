5cab8b557cd8b3ab944f993e10f531cf8ddb124a
Enzo Cornand <goulaheau@gmail.com>
2018-12-11 14:21:03 +0100

    Init

---
+++ Entity/RestEntity.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Goulaheau\RestBundle\Entity;
+
+abstract class RestEntity
+{
+}
---
+++ Repository/RestRepository.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Goulaheau\RestBundle\Repository;
+
+abstract class RestRepository
+{
+}
