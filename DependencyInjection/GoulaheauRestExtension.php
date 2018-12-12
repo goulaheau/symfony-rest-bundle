4c10b6ecfa9f187cc0957a01c9a97259d69e8cbd
Enzo Cornand <goulaheau@gmail.com>
2018-12-12 11:20:51 +0100

    Add service config

---
+++ DependencyInjection/GoulaheauRestExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Goulaheau\RestBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class GoulaheauRestExtension extends Extension
+{
+    /**
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
